3f7088049a30da15f3c0b8169397bb80516eee10
SohailV <sohail2889@live.com>
2022-01-29 10:57:50 -0500

    Working on Option

--- example/main.dart
+++ example/main.dart
@@ -4,4 +4,6 @@ void main() {
   var terminal = new Terminal();
   terminal.clearScreen();
   terminal.printPrompt('Hi there!');
+  var input = terminal.collectInput();
+  print('you just entered : $input');
 }
