d43eb94f768644718f3be01cbb8c9a916d98b19e
SohailV <sohail2889@live.com>
2022-01-29 11:14:34 -0500

    Testing Options

--- example/main.dart
+++ example/main.dart
@@ -1,9 +1,20 @@
 import 'package:prompter_sv/src/terminal.dart';
+import 'package:prompter_sv/src/option.dart';
 
 void main() {
   var terminal = new Terminal();
-  terminal.clearScreen();
-  terminal.printPrompt('Hi there!');
-  var input = terminal.collectInput();
-  print('you just entered : $input');
+
+  var options = [
+    new Option('I want red', '#f00'),
+    new Option('I want blue', '#00f'),
+  ];
+
+  terminal.printOptions(options);
+  var response = terminal.collectInput();
+  print(response);
+
+  // terminal.clearScreen();
+  // terminal.printPrompt('Hi there!');
+  // var input = terminal.collectInput();
+  // print('you just entered : $input');
 }
