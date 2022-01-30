ba8883ebd29e6c8945b7a3de27403fb79806317b
SohailV <sohail2889@live.com>
2022-01-29 19:27:06 -0500

    refactoring the code

--- example/main.dart
+++ example/main.dart
@@ -1,17 +1,29 @@
-import 'package:prompter_sv/src/terminal.dart';
+import 'package:prompter_sv/src/prompter.dart';
 import 'package:prompter_sv/src/option.dart';
 
 void main() {
-  var terminal = new Terminal();
+  //var terminal = new Terminal();
 
-  var options = [
+  final options = [
     new Option('I want red', '#f00'),
     new Option('I want blue', '#00f'),
   ];
 
-  terminal.printOptions(options);
-  var response = terminal.collectInput();
-  print(response);
+  final prompter = new Prompter();
+  print(prompter.askBinary('Do you like Dart'));
+
+  // final usersPick = prompter.askMultiple('What color do you like?', options);
+
+  // print(usersPick);
+
+  // final prompter = new Prompter();
+  // prompter.ask('What color do you want', options);
+  // prompter.ask('What color do you want', options);
+  // prompter.ask('What color do you want', options);
+
+  // terminal.printOptions(options);
+  // var response = terminal.collectInput();
+  // print(response);
 
   // terminal.clearScreen();
   // terminal.printPrompt('Hi there!');
